a2d57600481b42edb272f660119d8f7791a8d0f3
hkthomassen <h.thomassen@outlook.de>
2023-05-10 10:34:42 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Hanne Thomassen and is{" "}
-        <a
-          href="https://github.com/hkthomassen/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Hanne Thomassen and is{" "}
+          <a
+            href="https://github.com/hkthomassen/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
